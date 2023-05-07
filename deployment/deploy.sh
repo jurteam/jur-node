9ac6de19535badeb3bd10018a918c6acfd7bf7ef
MTM Suhail <getmtm@gmail.com>
2023-05-07 11:17:30 +0530

    fix(cicd): aura and granpa key definition added

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,6 +7,9 @@ if [ "$(docker ps -a -q)" ]; then
   docker rm $(docker ps -a -q)  # remove all containers
 fi
 
+sudo echo AURA_KEY="${${KEY_PREFIX}_AURA_KEY}" >> /etc/environment
+sudo echo GRANPA_KEY="${${KEY_PREFIX}_GRANPA_KEY}" >> /etc/environment
+
 pushd $DEPLOY_PATH && \
 gcloud --quiet auth activate-service-account --key-file=key.json && \
 gcloud --quiet auth configure-docker && \
--- deployment/start-jur-node.sh
+++ deployment/start-jur-node.sh
@@ -5,8 +5,8 @@ WS_PORT=9944
 RPC_PORT=9933
 SPEC_FILE_PATH="./spec.json"
 
-jur-node key insert --base-path  /tmp/node --chain $SPEC_FILE_PATH --scheme Sr25519 --suri $$AURA_KEY --key-type aura
-jur-node key insert --base-path  /tmp/node --chain $SPEC_FILE_PATH --scheme Ed25519 --suri $$GRANPA_KEY --key-type gran
+jur-node key insert --base-path  /tmp/node --chain $SPEC_FILE_PATH --scheme Sr25519 --suri $AURA_KEY --key-type aura
+jur-node key insert --base-path  /tmp/node --chain $SPEC_FILE_PATH --scheme Ed25519 --suri $GRANPA_KEY --key-type gran
 
 if [ "${BOOT_NODE}" == "TRUE" ]; then
     jur-node --base-path /tmp/node --chain $SPEC_FILE_PATH --port $PORT --ws-port $WS_PORT --rpc-port $RPC_PORT --validator --blocks-pruning archive --state-pruning archive --ws-max-connections 100 --unsafe-ws-external --rpc-cors all
