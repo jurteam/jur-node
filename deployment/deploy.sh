f08c91de519cac83f91e5f974760061e12084e7b
MTM Suhail <getmtm@gmail.com>
2023-05-07 11:52:54 +0530

    fix(cicd): updated deploy path

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DEPLOY_PATH=/home/circleci/deployment # NOTE: change this to your codebase location
+
 if [ "$(docker ps -a -q)" ]; then
   docker stop $(docker ps -a -q)  # stop all containers
   docker rm $(docker ps -a -q)  # remove all containers
@@ -14,7 +16,7 @@ echo $$AURA_KEY
 sudo echo AURA_KEY=$$AURA_KEY >> /etc/environment
 sudo echo GRANPA_KEY=$$GRANPA_KEY >> /etc/environment
 
-
+pushd $DEPLOY_PATH && \
 gcloud --quiet auth activate-service-account --key-file=key.json && \
 gcloud --quiet auth configure-docker && \
 
@@ -22,4 +24,5 @@ chmod +x ./start-jur-node.sh && \
 
 docker image prune -a -f && \
 docker-compose -f docker-compose-$NETWORK_TYPE.yml up -d && \
-echo "[$(date)] Successfully deployed" >> deploy.log
+echo "[$(date)] Successfully deployed" >> deploy.log && \
+popd
