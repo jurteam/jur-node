12c87b343635fc9c90ed0f32dfcf4838d46574a2
MTM Suhail <getmtm@gmail.com>
2023-05-07 11:50:42 +0530

    fix(cicd): pushd removed

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DEPLOY_PATH=$HOME/deployment # NOTE: change this to your codebase location
-
 if [ "$(docker ps -a -q)" ]; then
   docker stop $(docker ps -a -q)  # stop all containers
   docker rm $(docker ps -a -q)  # remove all containers
@@ -16,7 +14,7 @@ echo $$AURA_KEY
 sudo echo AURA_KEY=$$AURA_KEY >> /etc/environment
 sudo echo GRANPA_KEY=$$GRANPA_KEY >> /etc/environment
 
-pushd $DEPLOY_PATH && \
+
 gcloud --quiet auth activate-service-account --key-file=key.json && \
 gcloud --quiet auth configure-docker && \
 
@@ -24,5 +22,4 @@ chmod +x ./start-jur-node.sh && \
 
 docker image prune -a -f && \
 docker-compose -f docker-compose-$NETWORK_TYPE.yml up -d && \
-echo "[$(date)] Successfully deployed" >> deploy.log && \
-popd
+echo "[$(date)] Successfully deployed" >> deploy.log
