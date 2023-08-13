bea67fb8849f90e786c21498f3ae561261e401a3
Polkaverse <pankajchaudhary172@gmail.com>
2023-08-13 17:59:27 +0530

    added benchmarking

---
+++ pallets/whitelist/src/benchmarking.rs
@@ -0,0 +1,57 @@
+//! Benchmarking setup for pallet-whitelist
+//!
+#![cfg(feature = "runtime-benchmarks")]
+use super::*;
+
+#[allow(unused)]
+use crate::Pallet as Whitelist;
+use frame_benchmarking::v1::{benchmarks, whitelisted_caller};
+use frame_system::RawOrigin;
+
+fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
+	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
+}
+
+benchmarks! {
+	add_founder {
+		let caller: T::AccountId = whitelisted_caller();
+	}: _(
+		RawOrigin::Root,
+		caller.clone()
+	)
+	verify {
+		assert_last_event::<T>(Event::<T>::AddedUser(caller).into());
+	}
+
+	revoke_founder {
+		let caller: T::AccountId = whitelisted_caller();
+	}: _(
+		RawOrigin::Root,
+		caller.clone()
+	)
+	verify {
+		assert_last_event::<T>(Event::<T>::RevokedUser(caller).into());
+	}
+
+	add_admin {
+		let caller: T::AccountId = whitelisted_caller();
+	}: _(
+		RawOrigin::Root,
+		caller.clone()
+	)
+	verify {
+		assert_last_event::<T>(Event::<T>::AddedAdmin(caller).into());
+	}
+
+	revoke_admin {
+		let caller: T::AccountId = whitelisted_caller();
+	}: _(
+		RawOrigin::Root,
+		caller.clone()
+	)
+	verify {
+		assert_last_event::<T>(Event::<T>::RevokedAdmin(caller).into());
+	}
+
+	impl_benchmark_test_suite!(Whitelist, crate::mock::new_test_ext(), crate::mock::Test);
+}
