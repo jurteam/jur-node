ae5ec461bb69161373ff34d36b2329b0e370dcd6
0xMenna01 <emanuele.cryptdev@gmail.com>
2024-02-14 02:31:00 +0000

    Runtime integration of session pallet and validatorManager - session period 600

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -112,7 +112,7 @@ pub const MILLICENTS: Balance = 1_000_000_000;
 pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
 pub const DOLLARS: Balance = 100 * CENTS;
 
-pub const DEFAULT_SESSION_PERIOD: u32 = 50;
+pub const DEFAULT_SESSION_PERIOD: u32 = 600;
 
 pub const fn deposit(items: u32, bytes: u32) -> Balance {
 	items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
